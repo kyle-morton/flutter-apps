56cdbb96ea037b7e56489a4368e506b213e9db8a
Kyle Morton <kylemortondevelopment@gmail.com>
2020-03-30 22:36:58 -0500

    adding widget for infinite scroll

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -11,25 +11,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords()
     );
   }
 }
 
 /// State is what can change over lifetime of widget
 class RandomWordsState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[]; // the _ character enforces privacy in Dart lang
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  /// for each suggestion, builds out a row in the listview
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd)
+          return Divider(); // seperates each row
+
+        final index = i ~/ 2; // divides current row # by 2, returns floor()
+
+        // whenever you reach end of WordPairs, gen 10 more and add to list
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      } // itemBuilder
+    );
+  } 
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator')
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
 
